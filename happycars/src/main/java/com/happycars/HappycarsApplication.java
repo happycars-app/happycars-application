fc0f5da8960e79df98241180fa339cded3cbdbe1
bot <mdfarook2803@gmail.com>
2025-11-30 18:17:54 +0530

    remove empty README file  added teh layered structure for the backend

---
+++ happycars/src/main/java/com/happycars/HappycarsApplication.java
@@ -0,0 +1,14 @@
+package com.happycars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappycarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappycarsApplication.class, args);
+	}
+
+}
+
